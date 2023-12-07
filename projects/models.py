8d5b4703ca0f8b7cc536c871e69bc7077bfdcb0d
jeferson-viana <jefersonv28@gmail.com>
2023-12-07 17:06:11 -0300

    FEAT: Criando métodos em ProfileViewSet - (Requisito 4)

--- projects/models.py
+++ projects/models.py
@@ -17,7 +17,11 @@ class Project(models.Model):
     github_url = models.URLField(blank=False, max_length=500)
     keyword = models.CharField(blank=False, max_length=50)
     key_skill = models.CharField(blank=False, max_length=50)
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    profile = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        related_name="projects",
+    )
 
     def __str__(self):
         return self.name
--- projects/urls.py
+++ projects/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import ProfileViewSet, ProjectViewSet
+from .views import (
+  ProfileViewSet,
+  ProjectViewSet,
+)
 
 router = routers.DefaultRouter()
 router.register(r'profiles', ProfileViewSet)
