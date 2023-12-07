61d0b66646520cdbde200945ceda5a2b8a68f7d3
jeferson-viana <jefersonv28@gmail.com>
2023-12-07 15:47:21 -0300

    FEAT: Criando a model Project e o serializer - (Requisito 3)

--- projects/models.py
+++ projects/models.py
@@ -9,3 +9,15 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Project(models.Model):
+    name = models.CharField(blank=False, max_length=50)
+    description = models.TextField(blank=False, max_length=500)
+    github_url = models.URLField(blank=False, max_length=500)
+    keyword = models.CharField(blank=False, max_length=50)
+    key_skill = models.CharField(blank=False, max_length=50)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
