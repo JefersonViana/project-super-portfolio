1ae04ff6397a775ee1e839c56bd113404629a059
jeferson-viana <jefersonv28@gmail.com>
2023-12-08 12:47:52 -0300

    FEAT: Criandoos Serializers das models Certificate e CertifyingInstitution - (Requisito 5)

--- projects/serializers.py
+++ projects/serializers.py
@@ -26,24 +26,34 @@ class CertificateSerializer(serializers.ModelSerializer):
 
 
 class NestedCertificateSerializer(serializers.ModelSerializer):
+    # foi retirado o field profile e funcionou, porque?
     class Meta:
         model = Certificate
-        fields = ('id', 'name', 'timestamp', 'profiles')
+        fields = ('id', 'name')
 
 
 class CertifyingInstitutionSerializer(serializers.ModelSerializer):
-    certificates = NestedCertificateSerializer(many=True, read_only=True)
+    certificates = NestedCertificateSerializer(many=True)
 
     class Meta:
         model = CertifyingInstitution
         fields = ('id', 'name', 'url', 'certificates')
 
     def create(self, validated_data):
+        # mostra os dados que chega
+        # print(validated_data, 'VALIDADE DATA')
+
+        # retorna uma lista de [{"name": "certificado 1"},
+        # {"name": "certificado 2"}]
         certificates_data = validated_data.pop('certificates')
+
+        # criando a instituição certificadora
         certifying_institution = CertifyingInstitution.objects.create(
             **validated_data
         )
+        # for para criar os certificados e porque não tem o profile?
         for certificate_data in certificates_data:
+            # isso aqui funcionaria?
             # Certificate.objects.create(
             #     certifying_institution=certifying_institution,
             #     **certificate_data
